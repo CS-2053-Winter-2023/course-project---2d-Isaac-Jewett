7bb430258c1f5904e15fd20680370aaca408b031
AnthonyDurette <anthony.durette@gmail.com>
2023-04-11 15:32:01 -0700

    Torches
    Added a torch prefab and added torches into level 1. Also made the global light active until triggering the lore entry. Then the global light goes out.

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LightController : MonoBehaviour
 {
@@ -10,10 +11,16 @@ public class LightController : MonoBehaviour
     public GameObject playerLight;
     public GameObject flashlight;
     public GameObject globalLight;
+    public GameObject exitTrigger;
 
     private void Start()
     {
-        globalLight.SetActive(false);
+        if (!(SceneManager.GetActiveScene().name == "Room1")) {
+
+            globalLight.SetActive(false);
+
+        }
+        
     }
 
     // Update is called once per frame
@@ -28,6 +35,10 @@ public class LightController : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 270;
 
         flashlight.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (exitTrigger.activeInHierarchy) {
+            globalLight.SetActive(false);
+        }
         
 
     }
