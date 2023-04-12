a06044bea51753494a6e88559f06440434333c51
AnthonyDurette <anthony.durette@gmail.com>
2023-04-11 17:16:23 -0700

    Made Cover Opening Work in Journal

--- Assets/Scripts/EntryController.cs
+++ Assets/Scripts/EntryController.cs
@@ -30,6 +30,8 @@ public class EntryController : MonoBehaviour
 
     public CurrentEntry currentEntry;
 
+    private bool isDone;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -42,56 +44,96 @@ public class EntryController : MonoBehaviour
         entry7.SetActive(false);
         entry8.SetActive(false);
         cover.SetActive(false);
+        isDone = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isDone) {
+            StartCoroutine(JournalRoutine());
+        }
+
+    }
+
+    IEnumerator JournalRoutine() {
 
         if (currentEntry == CurrentEntry.entry1)
         {
-
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry1.SetActive(true);
-            
+            cover.SetActive(false);
+            isDone = true;
 
-        } else if (currentEntry == CurrentEntry.entry2) 
+        }
+        else if (currentEntry == CurrentEntry.entry2)
         {
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry2.SetActive(true);
+            cover.SetActive(false);
+            isDone = true;
 
         }
         else if (currentEntry == CurrentEntry.entry3)
         {
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry3.SetActive(true);
+            cover.SetActive(false);
+            isDone = true;
 
         }
         else if (currentEntry == CurrentEntry.entry4)
         {
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry4.SetActive(true);
+            cover.SetActive(false);
+            isDone = true;
 
         }
         else if (currentEntry == CurrentEntry.entry5)
         {
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry5.SetActive(true);
+            cover.SetActive(false);
+            isDone = true;
 
         }
         else if (currentEntry == CurrentEntry.entry6)
         {
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry6.SetActive(true);
+            cover.SetActive(false);
+            isDone = true;
 
         }
         else if (currentEntry == CurrentEntry.entry7)
         {
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry7.SetActive(true);
+            cover.SetActive(false);
+            isDone = true;
 
         }
         else if (currentEntry == CurrentEntry.entry8)
         {
+            cover.SetActive(true);
+            yield return new WaitForSeconds(1);
             entry8.SetActive(true);
+            cover.SetActive(false);
+            isDone = true;
 
         }
 
 
+        
     }
 
 
